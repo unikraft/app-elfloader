0eb7747f54b5b2fdb1462db31e503493633371d6
Tianyi Liu <i.pear@outlook.com>
2023-06-25 12:23:43 +0800

    VDSO syscall example

--- vdso/interface.c
+++ vdso/interface.c
@@ -4,12 +4,21 @@ __nsec (*ukplat_monotonic_clock)(void) = (__nsec (*)(void))KADDR_ukplat_monotoni
 __nsec (*ukplat_wall_clock)(void) = (__nsec (*)(void))KADDR_ukplat_wall_clock;
 
 unsigned long (*ukplat_tlsp_enter)(void) = (long unsigned int (*)(void))0x0000000000105380ULL;
-void (*ukplat_tlsp_exit)(unsigned long tlsp) = (void (*)(unsigned long))0x00000000001053f0dULL;
-unsigned long (*uk_syscall_r_getpid)(void) = (long unsigned int (*)(void))0x00000000001276e0ULL;
+void (*ukplat_tlsp_exit)(unsigned long tlsp) = (void (*)(unsigned long))0x00000000001053f0ULL;
+
+int (*uk_syscall_r_getpid)(void) = (int (*)(void))0x00000000001278a0ULL;
+int (*uk_syscall_r_getppid)(void) = (int (*)(void))0x0000000000127b40ULL;
 
 unsigned long uk_vdso_syscall_getpid() {
     unsigned long orig_tlsp = ukplat_tlsp_enter();
-    unsigned long ret = uk_syscall_r_getpid();
+    int ret = uk_syscall_r_getpid();
+    ukplat_tlsp_exit(orig_tlsp);
+    return ret;
+}
+
+unsigned long uk_vdso_syscall_getppid() {
+    unsigned long orig_tlsp = ukplat_tlsp_enter();
+    int ret = uk_syscall_r_getppid();
     ukplat_tlsp_exit(orig_tlsp);
     return ret;
 }
