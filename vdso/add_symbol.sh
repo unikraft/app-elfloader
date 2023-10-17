2aa3b709e49e8655e8a32937b7ad78a5207e6d02
Simon Kuenzer <simon@unikraft.io>
2023-10-18 01:52:36 +0200

    Makefile.uk: Make VDSO rules work for cross-compilation and Darwin
    This commit reworks the compilation rules for the VDSO so that they can be
    easily cross-compiled and be compiled on a Darwin environment (MacOS).
    The following improvements are implemented:
    - The build-system provided compiler and tool commands are called instead
      of calling the tools (e.g., `gcc`) directly which would create an
      incompatible VDSO when cross-compiling.
    - Any build artifact is created under the appelfloader build path (and
      not within the sources path).
    - Recompilation dependencies are correctly stated.
    - The step that adds symbol placeholders is separated from compiling
      the VDSO template.
    - Compiling and linking of the VDSO needs to be done with `-nostdlib`
      at both steps. In general, `-nostdlib` avoids the unnnecessary inclusion
      of any object files provided by a libc, like start files (e.g.,
      `crt0.o`). Those integrations are not needed for the VDSO because it
      provides only a way to lookup symbols and code that can be used by a libc
      (and dynamic linker) for performing faster system calls.
    
    Signed-off-by: Simon Kuenzer <simon@unikraft.io>

--- vdso/add_symbol.sh
+++ vdso/add_symbol.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+OBJCOPY_CMD=${OBJCOPY_CMD:-objcopy}
+CONF_FILE=${1}
+IN_FILE=${2}
+OUT_FILE=${3}
+
+cp -f "${IN_FILE}" "${OUT_FILE}"
 while IFS=' ' read -r vdso_symbol _; do
-  objcopy --add-symbol $vdso_symbol=.text:$VDSO_MAGIC_NUMBER,global,function $APPELFLOADER_BASE/vdso/libvdso.o
-  ((VDSO_MAGIC_NUMBER++))
-done < $APPELFLOADER_BASE/vdso/vdso_mapping.conf
+	"${OBJCOPY_CMD}" --add-symbol "${vdso_symbol}=.text:${VDSO_MAGIC_NUMBER},global,function" "${OUT_FILE}"
+	((VDSO_MAGIC_NUMBER++))
+done < "${CONF_FILE}"
